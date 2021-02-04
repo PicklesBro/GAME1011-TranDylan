a8106b90f94bb2e1c6a24cd14c99125635a2c60c
Dylan <talktodylantran@gmail.com>
2021-02-04 17:27:53 -0500

    Testing commit

--- GAME1011-Lab2-TranDylan/Main.cpp
+++ GAME1011-Lab2-TranDylan/Main.cpp
@@ -7,4 +7,6 @@ int main()
 	string menu = "Select a number to begin customization\n1. Create a new character\n2. View all characters\n3. Delete a character\n4. Exit";
 	cout << menu;
 	return 0;
+
+	// testing 
 }
